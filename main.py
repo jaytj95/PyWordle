59068a81e91a74e166896dbcda0bf79bc45aba17
Jason John <jaytj95@gmail.com>
2022-02-26 19:01:37 -0600

    wordle v1

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+import colorama
+from colorama import Fore, Style
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print("Welcome to PyWordle!")
+print("Initializing...", end='')
+
+boards = [
+    [("-", 0), ("-", 0), ("-", 0), ("-", 0), ("-", 0)],
+    [("-", 0), ("-", 0), ("-", 0), ("-", 0), ("-", 0)],
+    [("-", 0), ("-", 0), ("-", 0), ("-", 0), ("-", 0)],
+    [("-", 0), ("-", 0), ("-", 0), ("-", 0), ("-", 0)],
+    [("-", 0), ("-", 0), ("-", 0), ("-", 0), ("-", 0)]
+]
+answer = random.choice(open('wordle-answers-alphabetical.txt').read().splitlines())
+words = open('wordle-allowed-guesses.txt').read().splitlines()
+print("Done!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_board(game_round):
+    print("Round " + str(game_round))
+    for board in boards:
+        for i in range(5):
+            letter = board[i]
+            if letter[0] == "-" or letter[1] == 0:
+                color = Fore.LIGHTWHITE_EX
+            elif letter[1] == 1:
+                color = Fore.YELLOW
+            else:
+                color = Fore.GREEN
+            print(color, letter[0], end=' ')
+        print(Style.RESET_ALL)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_input():
+    guess = input("Enter your value: ")
+    # if guess not in words:
+    #     print("Invalid word, try a different one")
+    #     return get_input()
+    return guess
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# need to correct yellow markings: if yellow letter is solved, and there is not a duplicate, mark white
+def process_guess(game_round, guess):
+    board = []
+    for i in range(5):
+        c = guess[i]
+        a = answer[i]
+        if c == a:
+            board.append((c, 2))
+        elif c in answer:
+            board.append((c, 1))
+        else:
+            board.append((c, 0))
+    boards[game_round - 1] = board
+
+
+def play_game():
+    game_round = 1
+    guess = ""
+    while guess != answer and game_round < 6:
+        print_board(game_round)
+        guess = get_input()
+        process_guess(game_round, guess)
+        game_round = game_round + 1
+
+
+play_game()
+print_board(4)
+print(answer)
